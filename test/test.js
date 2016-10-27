dbe18d64e199612ae9e64734306d8f148a8d9ae7
Ed Mendoza <polyneue@gmail.com>
2016-10-27 11:22:01 -0400

    Troubleshooting travis

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@
 var Behance = require('../index.js');
 
 // Create an Instance of Behance with API Key
-var key = require('../api.json').key;
+var key = 123456789;
 var Be = new Behance(key);
 
 // Test Dependencies
